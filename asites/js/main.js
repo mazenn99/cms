4562ea297e437c7f5fa9e0d8a00a26e25568ffba
Mr.SToRM <mazenn99@gmail.com>
2023-06-05 23:12:56 +0300

    fix

---
+++ asites/js/main.js
@@ -0,0 +1,27 @@
+const ssvg = document.getElementById('ssvg');
+const navbar = document.getElementById('navbar');
+const minIcon = document.getElementById('minIcon');
+ document.getElementById('icon').onclick = function () {
+this.classList.toggle('change');
+navbar.classList.toggle('active');
+ssvg.classList.toggle('del');
+minIcon.classList.toggle('del-2');
+} 
+    
+  document.getElementById("ul-dropdown").onclick =  function() {
+    this.classList.toggle('change');
+  }
+  document.getElementById("ul-dropdown-Two").onclick =  function() {
+    this.classList.toggle('change');
+  }
+  document.getElementById("ul-dropdown-Three").onclick =  function() {
+    this.classList.toggle('change');
+  }
+  document.getElementById("ul-dropdown-Four").onclick =  function() {
+    this.classList.toggle('change');
+  }
+  document.getElementById("ul-dropdown-five").onclick =  function() {
+    this.classList.toggle('change');
+  }
+  
+
